7e69ca87b18a5a1a781bfdbdf7589bfe5e0387c6
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2025-12-04 15:16:46 +0200

    add the minimal mod code

---
+++ 42/media/java/client/src/Patch_MainScreenState.java
@@ -0,0 +1,18 @@
+package me.zed_0xff.zb_hello_world;
+
+import me.zed_0xff.zombie_buddy.Patch;
+import net.bytebuddy.asm.Advice;
+
+import zombie.ui.TextManager;
+import zombie.ui.UIFont;
+
+/**
+ * Patch the main menu background rendering to draw our Hello World text
+ */
+@Patch(className = "zombie.gameStates.MainScreenState", methodName = "renderBackground")
+public class Patch_MainScreenState {
+    @Advice.OnMethodExit
+    public static void exit() {
+        TextManager.instance.DrawString(UIFont.Medium, 0, 0, "Hello World from ZBHelloWorld!", 1.0, 1.0, 1.0, 1.0);
+    }
+}
